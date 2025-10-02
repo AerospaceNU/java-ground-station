9c6b93ff1a2f8eb5593c8d9ac42267cb7b10b6d8
namanp07 <patel.naman3@gmail.com>
2025-10-01 22:05:26 -0400

    Debugging for window

--- MainWindow.java
+++ MainWindow.java
@@ -1,19 +1,23 @@
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import java.awt.Window;
 
 public class MainWindow {
     JFrame frame = new JFrame();
+    Window window = new Window(frame);
     JButton button = new JButton();
 
-     public mainWindow()  {
+     public void mainWindow()  {
         button.setBounds(100,160,200,40);
         button.setFocusable(false);
-        button.addActionListener(this);
+        //button.addActionListener(this);
         frame.add(button);
         
+        frame.addNotify();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
-        frame.setLayout(null);
-        frame.setVisible(true);
+        window.setSize(400, 400);
+        window.setLayout(null);
+        window.setVisible(true);
+        window.toFront();
     }
 }
--- master.java
+++ master.java
@@ -1,7 +1,8 @@
 //import java-ground-station.MainWindow;
 
-public class main {
+public class master {
     public static void main(String[] args){
         MainWindow mainWindow = new MainWindow();
+        System.out.println(mainWindow.window.isShowing());
     }
 }
