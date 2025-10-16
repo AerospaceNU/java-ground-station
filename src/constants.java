39a27db7ae7f34d5cddfad1c494ebe001048fbb5
namanp07 <patel.naman3@gmail.com>
2025-10-15 22:07:03 -0400

    added serial lib and constants

---
+++ src/constants.java
@@ -0,0 +1,13 @@
+package src;
+import com.fazecast.jSerialComm.SerialPort;
+
+public class constants {
+    //all of these values need to be set since they are currently just placeholders
+public static final float GROUND_ELEVATION = 0.0f;
+public static final float GROUND_TEMPERATURE = 0.0f;
+public static final int BOARD_ORIENTATION = 0;
+public static final float MAIN_ELEVATION = 0.0f;
+public static final int DROGUE_DELAY = 1000;
+public static final float BATTERY_VOLTAGE_SENSOR_SCALE_FACTOR = 0.0f;
+public static final int PYRO_FIRE_DURATION = 0;
+}
