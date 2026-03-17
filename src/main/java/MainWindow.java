728f8d1ddeff1bacefaf2b1a4d4f12eaedfa9df8
Naman Patel <patel.naman3@gmail.com>
2026-03-17 00:22:56 -0400

    more efficient

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -32,9 +32,6 @@ public class MainWindow {
 	private final Map<String, SerialTab> activeTabs = new HashMap<>();
 	JPanel mainPanel = new JPanel();
 	private	JPanel	panel1 = new JPanel();
-	private	JPanel	panel2 = new JPanel();
-	private	JPanel	panel3 = new JPanel();
-	private	JPanel	panel4 = new JPanel();
 
 	public MainWindow() {
 		// build and show the UI
@@ -49,8 +46,6 @@ public class MainWindow {
 			tabbedPane.setBackground(Color.GREEN);
 			mainPanel.setBackground(Color.GRAY);
 			mainPanel.setLayout(new BorderLayout());
-			//mainPanel.add(tabbedPane, BorderLayout.CENTER);
-			//frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
 
 			frame.setLocationRelativeTo(null);
 			frame.setVisible(true);
@@ -75,17 +70,6 @@ public class MainWindow {
 				//tracks the tabs with activeTabs and adds a tab to the tabbedPane
 				activeTabs.put(portName, tab);
 				tabbedPane.addTab(portName, tab);
-
-				//sub tab content
-				/*subTab1Content.add(new JLabel("Content of Subtab 1"));
-				subTab2Content.add(console);
-    			subTab2Content.add(submitButton);*/
-
-				//sub tabs added to each tab
-				/*tabbedPane.add(subTabbedPane, tab);
-				subTabbedPane.addTab("Debug", subTab1Content);
-				subTabbedPane.addTab("Config", subTab2Content);
-				*/
 
 				// updates
 				tabbedPane.revalidate();
@@ -123,19 +107,6 @@ public class MainWindow {
         }
     }
 
-	//button gets triggered
-	/* 
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == submitButton) {
-			subTab2Content.add(l);
-			l.setText(console.getText());
-			/*cool.setHorizontalAlignment(SwingConstants.CENTER);
-				subTab2Content.add(cool, BorderLayout.CENTER);
-				mainPanel.revalidate();
-		}
-	} 
-	*/
-
 	//outdated code from previous implentation. It's still a useful reference for UI building!
 	public void itemTabPanel1()
 	{
@@ -147,55 +118,7 @@ public class MainWindow {
 		JButton btn2 = new JButton("Button 2");
 		btn2.setBounds(10, 45, 89, 23);
 		panel1.add(btn2);
-
-		/* String name = JOptionPane.showInputDialog("Enter your name:");
-
-        // Prompt the user for their age
-        String ageString = JOptionPane.showInputDialog("Enter your age:");
-        int age = Integer.parseInt(ageString); // Convert the string input to an integer
-
-        // Display the collected information using a message dialog
-        JOptionPane.showMessageDialog(null, "Hello, " + name + "!\nYou are " + age + " years old.");*/
-
 		
-	}
-
-	public void itemTabPanel2()
-	{
-		panel2 = new JPanel();
-		panel2.add(new JLabel("Tab 2 Content"));
-            panel2.add(new JButton("Button 3"));
-		
-		JButton btn4 = new JButton("Button 4");
-		btn4.setBounds(10, 45, 89, 23);
-		panel2.add(btn4);	
-	}
-
-	public void itemTabPanel3()
-	{
-		panel3.setLayout(null);;
-		
-		JButton btn5 = new JButton("Button 5");
-		btn5.setBounds(10, 11, 89, 23);
-		panel3.add(btn5);
-		
-		JButton btn6 = new JButton("Button 6");
-		btn6.setBounds(10, 45, 89, 23);
-		panel3.add(btn6);		
-		
-	}
-	
-	public void itemTabPanel4()
-	{
-		panel4.setLayout(null);;
-		
-		JButton btn7 = new JButton("Button 7");
-		btn7.setBounds(10, 11, 89, 23);
-		panel4.add(btn7);
-		
-		JButton btn8 = new JButton("Button 8");
-		btn8.setBounds(10, 45, 89, 23);
-		panel4.add(btn8);	
 	}
 
 }
